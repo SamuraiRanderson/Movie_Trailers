d5adb9f7a60379d644c10d2d4112f664cc0f67b1
Robert C Anderson <robtanderson4@gmail.com>
2016-03-12 21:53:28 -0600

    refactor: touch support

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -75,6 +75,20 @@ main_page_head = '''
               'frameborder': 0
             }));
         });
+        $(doument).on('taphold', '.hanging-close, .modal-backdrop, .modal',function(event){
+            $("#trailer-video-container").empty();
+        });
+
+        $(document).on("taphold", '.movie-tile', function (event) {
+            var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
+            var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
+            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
+              'id': 'trailer-video',
+              'type': 'text-html',
+              'src': sourceUrl,
+              'frameborder': 0
+            }));
+        });
         // Animate in the movies when the page loads
         $(document).ready(function () {
           $('.movie-tile').hide().first().show("fast", function showNext() {
